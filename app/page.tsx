11865f9294cd5daacf12fafd143191a251c5f35b
Catdrout <c4tdrout@protonmail.com>
2024-11-09 22:10:29 +0100

    i know the issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
 import { HeroSection } from '@/components/hero-section';
 import { FeaturesSection } from '@/components/features-section';
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
       <HeroSection />
-      <Analytics />
-      <SpeedInsights />
       <FeaturesSection />
     </main>
   );
